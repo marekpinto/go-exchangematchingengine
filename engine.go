4cd247689ba233266916247bb484698068c2b02b
jacob-stein1 <jmstein@bu.edu>
2023-03-12 16:54:24 +0800

    Create findMatch function

--- engine.go
+++ engine.go
@@ -12,25 +12,25 @@ import (
 
 type Engine struct{}
 
-func (e *Engine) accept(ctx context.Context, conn net.Conn, writeCh chan <- string, readCh <- chan instrument) {
+type CommandTuple struct {
+    cmd    inputType
+    id     uint32
+    price  uint32
+    count  uint32
+	exId   uint32
+}
+
+func (e *Engine) accept(ctx context.Context, conn net.Conn) {
 	go func() {
 		<-ctx.Done()
 		conn.Close()
 	}()
-	go handleConn(conn, writeCh chan string, readCh chan instrument)
+	go handleConn(conn)
 }
 
-func handleConn(conn net.Conn, writeCh chan <- instrument, readCh <- chan string) {
+func handleConn(conn net.Conn) {
 	defer conn.Close()
 	for {
-		select {
-		case msg := <-ch:
-			fmt.Printf("Received message %d\n", msg)
-			// Do something with the message
-		default:
-			// If there is no message to read, sleep for a bit
-			time.Sleep(100 * time.Millisecond)
-		}
 		in, err := readInput(conn)
 		if err != nil {
 			if err != io.EOF {
@@ -53,4 +53,96 @@ func handleConn(conn net.Conn, writeCh chan <- instrument, readCh <- chan string
 
 func GetCurrentTimestamp() int64 {
 	return time.Now().UnixNano()
+}
+
+func findMatch(cmd inputType, price uint32, count uint32, activeID uint32, timestamp int64) {
+    switch cmd {
+		
+    case inputBuy: {
+		sellPrice := price
+		bestIndex := -1
+		amt := count
+		// tickerSlice := slice stored in ticker goroutine
+
+		// Find best match
+		for i := 0; i < len(tickerSlice); i++ {
+			if (tickerSlide[i].cmd == inputSell && tickerSlice[i].price <= price) {
+				sellPrice = tickerSlice[i].price
+				bestIndex = i
+			}
+		}
+
+		// If a match is found...
+		if bestIndex != -1 {
+
+			tickerSlice[bestIndex].exId += 1
+
+			// Active order has higher count
+			if (amt >= tickerSlice[bestIndex].count) {
+				amt = amt - tickerSlice[bestIndex].count
+				outputOrderExecuted(tickerSlice[bestIndex].id, activeID, sellPrice, tickerSlide[bestIndex].count, GetCurrentTimestamp())
+				// tickerSlice.remove(bestIndex)
+			}
+
+			// Resting order has higher count
+			else {
+				tickerSlice[bestIndex].count -= amt
+				outputOrderExecuted(tickerSlice[bestIndex].id, activeID, sellPrice, amt, GetCurrentTimestamp())
+				amt = 0
+			}
+		}
+
+		/*
+			If amt = 0, order was settled
+			If amt = prev, order found no match
+			if amt < prev, order matched with resting and looks again
+		*/
+		return amt
+	}
+        
+    case inputSell: {
+		buyPrice := price
+		bestIndex := -1
+		amt := count
+		// tickerSlice := slice stored in ticker goroutine
+
+		// Find best match
+		for i := 0; i < len(tickerSlice); i++ {
+			if (tickerSlide[i].cmd == inputSell && tickerSlice[i].price >= price) {
+				buyPrice = tickerSlice[i].price
+				bestIndex = i
+			}
+		}
+
+		// If a match is found...
+		if bestIndex != -1 {
+
+			tickerSlice[bestIndex].exId += 1
+
+			// Active order has higher count
+			if (amt >= tickerSlice[bestIndex].count) {
+				amt = amt - tickerSlice[bestIndex].count
+				outputOrderExecuted(tickerSlice[bestIndex].id, activeID, sellPrice, tickerSlide[bestIndex].count, GetCurrentTimestamp())
+				// tickerSlice.remove(bestIndex)
+			}
+
+			// Resting order has higher count
+			else {
+				tickerSlice[bestIndex].count -= amt
+				outputOrderExecuted(tickerSlice[bestIndex].id, activeID, sellPrice, amt, GetCurrentTimestamp())
+				amt = 0
+			}
+		}
+
+		/*
+			If amt = 0, order was settled
+			If amt = prev, order found no match
+			if amt < prev, order matched with resting and looks again
+		*/
+		return amt
+	}
+
+    default:
+        fmt.Println("Invalid command type")
+    }
 }
